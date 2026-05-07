f4414c1e195ceb51f6ab6814964b1e32c7107d4b
donodj <donovan.jab0613@gmail.com>
2026-05-07 12:17:29 -0700

    remove old function from main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import gui
 
 if __name__ == "__main__":
-    # gui.play_game(size=3, blanks=40)  ## runs game in terminal
-        gui.run_compare_gui()  ## runs game in seperate window
+    gui.run_compare_gui()
